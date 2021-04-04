4d93b617da12c21dc8ffde924d5eb67280135eae
Vanessa G <vlopez_jv@hotmail.com>
2021-04-03 22:56:17 -0700

    new files

--- client/src/components/Config.js
+++
@@ -1,8 +0,0 @@
-//SERVER ROUTES
-export const USER_SERVER = '/api/users';
-
-export const API_KEY = '9016503e1d2a675daf0a207857591e20';
-export const API_URL = 'https://api.themoviedb.org/3/';
-
-export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
-
--- server/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://<VSLG2021b>:<KrYkQTjWe57omYoh>@cluster0.85jyo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}

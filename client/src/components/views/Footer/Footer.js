a6f9a769c463a69d9e34f794beb7e38d4337fb02
Vanessa G <vlopez_jv@hotmail.com>
2021-04-05 22:09:13 -0700

    added missing files and deleted nodemodules

---
+++ client/src/components/views/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Icon} from 'antd';
+
+function Footer() {
+    return (
+        <div style={{
+            height: '80px', display: 'flex',
+            flexDirection: 'column', alignItems: 'center',
+            justifyContent: 'center', fontSize:'1rem'
+        }}>
+           <p> Contact Us</p>
+        </div>
+    )
+}
+
+export default Footer

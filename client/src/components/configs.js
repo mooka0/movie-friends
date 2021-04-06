0007cfecec919c6fff88684f05ce9cf307641fec
Vanessa G <vlopez_jv@hotmail.com>
2021-04-05 22:43:39 -0700

    reinstated mongoDBApi in config and dev

--- client/src/components/configs.js
+++ client/src/components/configs.js
@@ -1,7 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
 
-export const API_KEY = '9016503e1d2a675daf0a207857591e20';
+export const API_KEY = 'e7d2850ec39191d3ec4303b8eb99765e';
 export const API_URL = 'https://api.themoviedb.org/3/';
 
 export const IMAGE_URL = 'http://image.tmdb.org/t/p/';

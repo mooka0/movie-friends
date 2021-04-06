1ac8f5360c25210ce4b4962d0038cf7e04866cdc
Vanessa G <vlopez_jv@hotmail.com>
2021-04-05 23:25:20 -0700

    added serviceWorker, and debugged. Working front end. Login/signup still error

--- client/src/components/configs.js
+++
@@ -1,7 +0,0 @@
-//SERVER ROUTES
-export const USER_SERVER = '/api/users';
-
-export const API_KEY = 'e7d2850ec39191d3ec4303b8eb99765e';
-export const API_URL = 'https://api.themoviedb.org/3/';
-
-export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
